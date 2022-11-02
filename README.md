f7a29819dd02f1f3ee812a17eefe4eafdd70cb5e
Mykhaylo Kotyk <mikl.kotyk@gmail.com>
2022-11-02 20:17:35 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PackageContactToCloud
+
+A description of this package.
---
+++ Sources/PackageContactToCloud/PackageContactToCloud.swift
@@ -0,0 +1,6 @@
+public struct PackageContactToCloud {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
